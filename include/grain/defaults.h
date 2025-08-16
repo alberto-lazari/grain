bae3736b7f2bd96f048bcdb32dd61c915b58d41f
Alberto Lazari <alberto.lazari@gmail.com>
2025-08-16 18:52:04 +0200

    Use typed defaults

--- include/grain/defaults.h
+++ include/grain/defaults.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <cstddef>
+
 // Maximum size in bytes of objects that can fit in a Grain
-#define DEFAULT_MAX_GRAIN_SIZE 64
+constexpr std::size_t DEFAULT_MAX_GRAIN_SIZE = 64;
 
 // Default number of bytes that get allocated for a handful.
 // Big values of grain size may result in huge Handfuls, so this is an upper limit.
 // However, small values of grain size may result in Handful::Count overflows,
 // so this is not always enforced.
-#define DEFAULT_HANDFUL_SIZE 4096
+constexpr std::size_t DEFAULT_HANDFUL_SIZE = 4096;
