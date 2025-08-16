81e41f3cbbad4e8cc2e2a7f4d93e1d3258982c30
Alberto Lazari <alberto.lazari@gmail.com>
2025-08-16 11:18:11 +0200

    STL allocator compliance

--- include/grain/Allocator.h
+++ include/grain/Allocator.h
@@ -11,6 +11,8 @@ namespace grain
 
 /**
  * STL-compliant allocator using the global Granary.
+ * Satisfies requirements listed here:
+ * https://en.cppreference.com/w/cpp/named_req/Allocator
  */
 template <typename T,
           std::size_t max_grain_size = DEFAULT_MAX_GRAIN_SIZE,
@@ -20,29 +22,47 @@ struct Allocator
     using value_type = T;
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using void_pointer = void*;
+    using const_void_pointer = const void*;
+
+    Allocator() noexcept = default;
+
+    template <typename U>
+    constexpr Allocator(const Allocator<U, max_grain_size, handful_size>&) noexcept
+    {
+    }
 
     template <typename U>
     struct rebind { using other = Allocator<U, max_grain_size, handful_size>; };
 
+    static constexpr size_type max_size() noexcept
+    {
+        return std::numeric_limits<size_type>::max() / sizeof(value_type);
+    }
+
     /**
      * Non-standard noexcept allocation.
      */
-    T* _allocate_nothrow(const std::size_t n) noexcept
+    static pointer _allocate_nothrow(const size_type n) noexcept
     {
-        void* const p = TheGranary::reach().pick(sizeof(T) * n);
-        return static_cast<T*>(p);
+        void* const p = TheGranary::reach().pick(sizeof(value_type) * n);
+        return static_cast<pointer>(p);
     }
 
-    T* allocate(const std::size_t n)
+    static pointer allocate(const size_type n)
     {
-        void* const p = TheGranary::reach().pick(sizeof(T) * n);
+        if (n > max_size()) throw std::bad_array_new_length();
+
+        void* const p = TheGranary::reach().pick(sizeof(value_type) * n);
         if (!p) throw std::bad_alloc();
-        return static_cast<T*>(p);
+        return static_cast<pointer>(p);
     }
 
-    void deallocate(T* const p, const std::size_t n) noexcept
+    static void deallocate(const pointer p, const size_type n) noexcept
     {
-        const bool all_well = TheGranary::reach().put_back(p, sizeof(T) * n);
+        const bool all_well = TheGranary::reach().put_back(p, sizeof(value_type) * n);
         assert(all_well);
     }
 
@@ -52,7 +72,7 @@ struct Allocator
     template <typename U>
     constexpr bool operator!=(const Allocator<U>& other) const noexcept
     {
-        return !(*this == other);
+        return !operator==(other);
     }
 
 private:
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,15 +1,53 @@
 #include "grain.h"
 
+// Allocator
 void test_simple_alloc_dealloc()
 {
-    grain::Allocator<int> alloc;
-    int* n = alloc.allocate(1);
-    alloc.deallocate(n, 1);
+    int* p = grain::Allocator<int>::allocate(1);
+    grain::Allocator<int>::deallocate(p, 1);
+}
+
+void test_stl_compliance()
+{
+    using T = int;
+    using A = grain::Allocator<T>;
+    const A a;
+    constexpr A::size_type n = 5;
+
+    const A::size_type max_size = a.max_size();
+    assert(sizeof(A::value_type) * n <= max_size);
+
+    A::pointer p = a.allocate(n);
+    A::const_pointer cp = p;
+    A::value_type& r = *p;
+    const A::value_type& cr = *cp;
+
+    A::void_pointer vp = p;
+    A::const_void_pointer cvp = cp;
+    assert(static_cast<A::pointer>(vp) == p);
+    assert(static_cast<A::const_pointer>(cvp) == cp);
+
+    a.deallocate(p, n);
+
+    A a1(a);
+    assert(a1 == a);
+
+    using U = char;
+    using B = grain::Allocator<U>;
+    B b;
+    A a2(b);
+    assert(B(a) == b && A(b) == a);
+
+    A a3(std::move(a));
+    assert(a3 == a);
+
+    A a4(std::move(b));
 }
 
 int main()
 {
     test_simple_alloc_dealloc();
+    test_stl_compliance();
 
     return 0;
 }
